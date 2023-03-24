7a01e9724ce7b8fcb045b5178345517fdf9afc42
base41jrma <base41jrma@gmail.com>
2023-03-23 20:44:07 -0600

    Cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+     apiKey: "AIzaSyD0x7kXXEYsQiUvYJTU_zCVT151NWKwv74",
+    authDomain: "base41jrma22.firebaseapp.com",
+    projectId: "base41jrma22",
+    storageBucket: "base41jrma22.appspot.com",
+    messagingSenderId: "764036096281",
+    appId: "1:764036096281:web:c960370291b51f936aa588",
+    measurementId: "G-F0J7NPCHL1"
+});
